af25805e3be628d5a92e685ce51a79cb93d2d4d2
yoav-es <yoav911gt1@gmail.com>
2022-01-29 18:57:43 +0200

    fourth

--- git_rep/coder.sv
+++ git_rep/coder.sv
@@ -7,41 +7,59 @@ input logic [3:0] r_counter,
 input logic [3:0] w_counter,
 output logic out);
 
-logic inputenable,
-logic clearen,
-logic delayen,
-logic [29:0] timer 
-logic [3:0] mem,
+logic clearen;
+logic delayen;
+logic disa;
+logic [29:0] timer; 
+logic [3:0] mem;
+logic [2:0] inp;
 
 
+//input
+always_ff @ (posedge clk or negedge rst)
+	if(~rst)
+		inp <= 3'b000;
+	else if (disa):
+		inp <= inp;
+	else	
+		inp <= {in, inp[2:1]};
+		
+//memory
+always_ff @ (posedge clk or negedge rst)
+	if(~rst)
+		mem <= 4'b0000;
+	else
+		if(clearen)
+			mem  <= {in,inp};
+		
 //clear_counter
 always_ff @ (posedge clk or negedge rst)
 	if(~rst)
-		r_counter <= 4'b0000;
+		r_counter <= 3'b000;
 	else if (in == mem):
-		r_counter <= r_counter + 4'b0001;
+		r_counter <= r_counter + 3'b001;
 	else	
-		r_counter <=  4'b0000;
+		r_counter <= 3'b000;;
 		
 // wrong counter 
 always_ff @ (posedge clk or negedge rst)
 	if(~rst)
-		w_counter <= 4'b0000;
+		w_counter <= 3'b000;
 	else if (in != mem):
-		w_counter <= r_counter + 4'b0001;
+		w_counter <= r_counter + 3'b001;
 	else	
-		w_counter <= 4'b0000;
+		w_counter <= 3'b000;
 
+//timer
 always_ff @ (posedge clk or negedge rst)
 	if(~rst)
 		timer <= 5'b00000;
-	else if (delayen)
-		time - 
-	else timer <= 5'b00000;
+	else if (~delayen)
+		timer <= 5'b00000;
+	else if(~disa)
+		timer <= timer + 5'b00001;
 	
-	
-assign inputen = (delayen == 1) ? 1 : 0; 	
-	
+		
 // clearm mem 
 assign clearen = (r_counter == 3'b100) ? 1 : 0; 
 
@@ -51,5 +69,7 @@ assign delayen = (w_counter == 3'b100) ? 1 : 0;
 // control lock 
 assign out = (mem == in) ? 1'b1 : 1'b0;
 
+// timer disable
+assign disa = (timer == 31) ? 1'b0: 1'b1;
 
 endmodule // coder
